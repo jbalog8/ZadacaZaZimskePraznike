61ae85eda342cff8e845cf3768fb2dd3ce966c78
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2021-12-24 22:16:49 +0100

    Doing some homework

---
+++ ZadacaZaZimskePraznike/src/Z05.java
@@ -0,0 +1,20 @@
+package edunovaZadaca;
+
+public class Z05 {
+	
+	public static void main(String[] args) {
+//		Write a Java program that takes two numbers as input and display the product of two numbers. Go to the editor
+//		Test Data:
+//		Input first number: 25
+//		Input second number: 5
+//		Expected Output :
+//		25 x 5 = 125
+		
+		int a = 25;
+		int b = 5;
+		int mult = a*b;
+		System.out.println(mult);
+		
+	}
+
+}
---
+++ ZadacaZaZimskePraznike/src/Z06.java
@@ -0,0 +1,36 @@
+package edunovaZadaca;
+
+public class Z06 {
+
+	public static void main(String[] args) {
+
+//		Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers. Go to the editor
+//		Test Data:
+//		Input first number: 125
+//		Input second number: 24
+//		Expected Output :
+//		125 + 24 = 149
+//		125 - 24 = 101
+//		125 x 24 = 3000
+//		125 / 24 = 5
+//		125 mod 24 = 5
+
+		int a = 125;
+		int b = 24;
+		int sum = a + b;
+		int sub = a - b;
+		int mult = a * b;
+		int div = a / b;
+		int mod = a % b;
+		
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(mult);
+		System.out.println(div);
+		System.out.println(mod);
+		
+		
+
+	}
+
+}
