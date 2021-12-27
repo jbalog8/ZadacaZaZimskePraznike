38c861e33a4f12cef7a8d4a5a9884d6a68ec7ee2
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2021-12-27 12:43:46 +0100

    Doing some winter homework

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Z13.java
@@ -0,0 +1,27 @@
+package edunovaZadaca;
+
+
+
+public class Z13 {
+	
+	public static void main(String[] args) {
+//		Write a Java program to print the area and perimeter of a rectangle. Go to the editor
+//		Test Data:
+//		Width = 5.5 Height = 8.5
+//
+//		Expected Output
+//		Area is 5.6 * 8.5 = 47.60
+//		Perimeter is 2 * (5.6 + 8.5) = 28.20
+		
+		double width = 5.5;
+		double height = 8.5;
+		
+		double area = width * height;
+		double perimeter = 2 *(width + height);
+		
+		System.out.println(area);
+		System.out.println(perimeter);
+		
+	}
+
+}
---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Z16.java
@@ -0,0 +1,24 @@
+package edunovaZadaca;
+
+public class Z16 {
+	
+	public static void main(String[] args) {
+//		Write a Java program to print a face. Go to the editor
+//		Expected Output
+//
+//		 +"""""+                                                 
+//		[| o o |]                                                
+//		 |  ^  |                                                 
+//		 | '-' |                                                 
+//       +-----+
+		
+		 System.out.println(" +\"\"\"\"\"+ ");
+	        System.out.println("[| o o |]");
+	        System.out.println(" |  ^  | ");
+	        System.out.println(" | '-' | ");
+	        System.out.println(" +-----+ ");
+}
+		
+		
+
+}
