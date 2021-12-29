360618f79e8a5c91cb1cb67fe5c60ea8b3d477b0
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2021-12-29 21:56:24 +0100

    Creating spiral matrix

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/CT_try008.java
@@ -0,0 +1,14 @@
+package edunovaZadaca;
+
+import javax.swing.JOptionPane;
+
+public class CT_try008 {
+	
+	public static void main(String[] args) {
+		int red = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redaka"));
+		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupac"));
+
+		int[][] matrica = new int[red][stupac];
+	}
+
+}
