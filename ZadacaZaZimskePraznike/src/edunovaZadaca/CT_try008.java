85c95c18708b1bfd4ea1321fada15adc30bb6fe5
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2021-12-31 15:08:22 +0100

    Creating spiral matrix
    Creating spiral matrix after 8 tries

--- ZadacaZaZimskePraznike/src/edunovaZadaca/CT_try008.java
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/CT_try008.java
@@ -10,15 +10,53 @@ public class CT_try008 {
 
 		int[][] matrica = new int[red][stupac];
 		
-
-		for(int i = 0; i < red; i++) {
-			for(int j = 0; j < stupac; j++){
+		
+		int suma = 1, a = 0, a2 = stupac -1, b = 0, b2 = red -1;
+		
+		while(suma <= red*stupac) { 						//s desna na lijevo
+			for(int i = a2; i >= a; i--) {
+				matrica[b2][i] = suma;
+				suma ++;
+				if(suma > red * stupac) {
+					break;
+					}
+			}
+			for (int i= b2 -1; i >= b; i--) {				//od dolje prema gore
+				matrica[i][a] = suma;
+				suma ++;
+				if(suma > red * stupac) {
+					break;
+				}
+			}
+			for(int i = a +1; i <= a2; i++) {				//s lijeva na desno
+				matrica[a][i] = suma;
+				suma ++;
+				if(suma > red * stupac) {
+					break;
+				}
+			}
+			for (int i = b +1; i < b2; i++) {				//od gore prema dolje
+				matrica[i][a2] = suma;
+				suma ++;
+				if(suma > red * stupac) {
+					break;
+				}
+			}
+			
+			b2--;
+			a2 --;
+			b++;
+			a++;
+		}
+		
+			for(int i = 0; i < red; i++) {
+				for(int j = 0; j < stupac; j++){
 				System.out.print(matrica[i][j] + " \t");
 			}
 			System.out.println();
 			
 	}
-	}
+}
 }
 
 
