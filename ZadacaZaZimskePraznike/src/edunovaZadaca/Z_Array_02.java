d86c6ffd7a14e60752830314f07ad3a91624e615
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-01-03 13:57:38 +0100

    Doing some homwork

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Z_Array_02.java
@@ -0,0 +1,16 @@
+package edunovaZadaca;
+
+public class Z_Array_02 {
+	public static void main(String[] args) {
+		
+		//2. Write a Java program to sum values of an array
+		
+		int [] array = {1,2,3,4,5,6,7,8,9,10};
+		int suma = 0;
+		for(int i: array)
+			suma += i;
+		System.out.println(suma);
+	}
+	
+
+}
