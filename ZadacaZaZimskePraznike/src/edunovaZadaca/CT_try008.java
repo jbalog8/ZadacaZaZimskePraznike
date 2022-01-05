68362310a7146b897c620fcc179ca1a2c6c0d0cf
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-01-05 23:54:04 +0100

    Modifying code
    Modifying code for easier understanding

--- ZadacaZaZimskePraznike/src/edunovaZadaca/CT_try008.java
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/CT_try008.java
@@ -11,42 +11,47 @@ public class CT_try008 {
 		int[][] matrica = new int[red][stupac];
 		
 		
-		int suma = 1, a = 0, a2 = stupac -1, b = 0, b2 = red -1;
+		int k = 1;
+		int c1 = 0; // pocetka stupca
+		int c2 = stupac - 1; // kraj stupca
+		int r1 = 0; // pocetak reda
+		int r2 = red - 1; // kraj reda
 		
-		while(suma <= red*stupac) { 						//s desna na lijevo
-			for(int i = a2; i >= a; i--) {
-				matrica[b2][i] = suma;
-				suma ++;
-				if(suma > red * stupac) {
+		
+		while(k <= red*stupac) { 						//s desna na lijevo
+			for(int i = c2; i >= c1; i--) {
+				matrica[r2][i] = k++;
+				
+				if(k > red * stupac) {
 					break;
 					}
 			}
-			for (int i= b2 -1; i >= b; i--) {				//od dolje prema gore
-				matrica[i][a] = suma;
-				suma ++;
-				if(suma > red * stupac) {
+			for (int i= r2 -1; i >= r1; i--) {				//od dolje prema gore
+				matrica[i][c1] = k++;
+				
+				if(k > red * stupac) {
 					break;
 				}
 			}
-			for(int i = a +1; i <= a2; i++) {				//s lijeva na desno
-				matrica[a][i] = suma;
-				suma ++;
-				if(suma > red * stupac) {
+			for(int i = c1 +1; i <= c2; i++) {				//s lijeva na desno
+				matrica[c1][i] =k++ ;
+				
+				if(k > red * stupac) {
 					break;
 				}
 			}
-			for (int i = b +1; i < b2; i++) {				//od gore prema dolje
-				matrica[i][a2] = suma;
-				suma ++;
-				if(suma > red * stupac) {
+			for (int i = r1 +1; i < r2; i++) {				//od gore prema dolje
+				matrica[i][c2] = k++;
+				
+				if(k > red * stupac) {
 					break;
 				}
 			}
 			
-			b2--;
-			a2 --;
-			b++;
-			a++;
+			c2--;
+			r2 --;
+			r1++;
+			c1++;
 		}
 		
 			for(int i = 0; i < red; i++) {
