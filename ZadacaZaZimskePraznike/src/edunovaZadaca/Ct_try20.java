85d57fb8cab43c3c92e53c95fd046e44b310efec
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-01-05 23:24:54 +0100

    Creating diagonal matrix
    Works only with the same numbers
    For example 5x5

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Ct_try20.java
@@ -0,0 +1,53 @@
+package edunovaZadaca;
+
+import javax.swing.JOptionPane;
+
+public class Ct_try20 {
+
+	public static void main(String[] args) {
+
+		int m = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj redova"));
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj stupaca"));
+		int[][] matrix = new int[m][n];
+		int suma = 1;
+		
+
+		
+		for (int k = 0; k <= m - 1; k++) {
+			int i = k;
+			int j = 0;
+
+			while (i >= 0) {
+				matrix[i][j] = suma++;
+				
+				i = i - 1;
+				j = j + 1;
+				
+				
+				}
+			}
+			
+		
+
+		for (int k = 1; k <= n - 1; k++) {
+			int i = m - 1;
+			int j = k;
+
+			while (j <= n - 1) {
+				matrix[i][j] = suma++;
+				
+				i = i - 1;
+				j = j + 1;
+			}
+			
+		}
+		
+		
+		for (int i = 0; i < n ; i++) {
+			for (int j = 0; j < m ; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
