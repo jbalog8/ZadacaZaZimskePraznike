d2259cfcacb0f5540fe610eda2154cea389478b5
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-01-08 16:08:32 +0100

    Doing some homework
    I don't know how to do this task

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Zadatci Conditional Statemen/Z_Conditional_Statement02.java
@@ -0,0 +1,18 @@
+package edunovaZadaca;
+
+import javax.swing.JOptionPane;
+
+public class Z_Conditional_Statement02 {
+	
+	public static void main(String[] args) {
+		
+		double a = Double.parseDouble(JOptionPane.showInputDialog("Unesi prvi broj"));
+		double b = Double.parseDouble(JOptionPane.showInputDialog("Unesi treci broj"));
+		double c = Double.parseDouble(JOptionPane.showInputDialog("Unesi treci broj"));
+		double korjen = b * b - 4.0 * a * c;
+		
+		
+		
+	}
+
+}
