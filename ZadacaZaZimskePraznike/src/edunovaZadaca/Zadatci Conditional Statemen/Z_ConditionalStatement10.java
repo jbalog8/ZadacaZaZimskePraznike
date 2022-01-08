e3d2df0a311dae5a40e410981494436408c9e4a2
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-01-08 16:53:11 +0100

    Doing some homework

---
+++ ZadacaZaZimskePraznike/src/edunovaZadaca/Zadatci Conditional Statemen/Z_ConditionalStatement10.java
@@ -0,0 +1,14 @@
+package edunovaZadaca;
+
+public class Z_ConditionalStatement10 {
+	
+	public static void main(String[] args) {
+		
+		int i ;
+		for(i =1; i <= 10; i++ ) {
+			System.out.print(i + "\n");
+		}
+	
+	}
+
+}
